8bd42d22378de856395c8888e32ac459fff10da5
seokgeun_cho <choseokgeun@gmail.com>
2025-05-14 17:15:12 +0900

    커밋

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,7 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ko">
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>나의 소개페이지지</h1>
-      <img
-        src="C:\Users\playdata\Desktop\d5fb3873f5ef8090cf92e59c72cbff2d.png"
-        alt="베스킨 라빈스 이미지"
-      />
+      <h1>나의 소개페이지</h1>
     </div>
   );
 }
